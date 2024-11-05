98a67798814c73efc22822b987b7d98488723e31
laukeshBS <laukesh.kumar@bharuwasolutions.com>
2024-11-05 12:26:01 +0530

    VAPT issues fixed

--- app/Http/Controllers/Cms/MenuController.php
+++ app/Http/Controllers/Cms/MenuController.php
@@ -403,8 +403,71 @@ class MenuController extends BaseController
     //         $this->updateMediaUrls($childMenu, $baseUrl);
     //     }
     // }
-    public function lang_slugs_wise(Request $request): JsonResponse
-    {
+    // public function lang_slugs_wise(Request $request): JsonResponse{
+    //     $data = $request->all();
+    //     $menu_url = $data['menu_url'] ?? null;
+       
+    //     $lang_code = $data['lang_code'] ?? null;
+    //     //dd($data);
+    //     if (!$lang_code) {
+    //         return $this->sendError('Lang code parameter is missing.', 400);
+    //     }
+      
+    //     // Retrieve all matching menu items, not just the first one
+    //     $menus = Menu::where('language_id', $lang_code)->where('approve_status', 3)
+    //         ->where('menu_url', $menu_url)->with(['children' => function ($query) {
+    //             $query->where('approve_status', 3)
+    //                   ->orderBy('page_order', 'ASC');
+    //         }])
+    //         ->select(
+    //             'menu_child_id',
+    //             'menu_name',
+    //             'menu_url',
+    //             'menu_title',
+    //             'menu_keyword',
+    //             'menu_description',
+    //             'content',
+    //             'doc_upload',
+    //             'menu_links',
+    //             'welcomedescription',
+    //             'banner_img',
+    //             'img_upload'
+    //         )->orderBy('page_order','ASC')
+    //         ->get(); // Use get() to retrieve all matching records
+
+    //         if ($menus->isNotEmpty()) {
+    //             $baseUrl = url('public/uploads/admin/cmsfiles/menus/');
+    
+    //             // Append base URL to media paths in the `children` relationship
+    //             $menus->each(function ($menu) use ($baseUrl) {
+    //                 if ($menu->doc_upload) {
+    //                     $menu->doc_upload = $baseUrl . '/' . $menu->doc_upload;
+    //                 }
+    //                 if ($menu->img_upload) {
+    //                     $menu->img_upload = $baseUrl . '/' . $menu->img_upload;
+    //                 }
+    //                 if ($menu->banner_img) {
+    //                     $menu->banner_img = $baseUrl . '/' . $menu->banner_img;
+    //                 }
+    //                 $menu->children->each(function ($childMenu) use ($baseUrl) {
+    //                     if ($childMenu->doc_upload) {
+    //                         $childMenu->doc_upload = $baseUrl . '/' . $childMenu->doc_upload;
+    //                     }
+    //                     if ($childMenu->img_upload) {
+    //                         $childMenu->img_upload = $baseUrl . '/' . $childMenu->img_upload;
+    //                     }
+    //                     if ($childMenu->banner_img) {
+    //                         $childMenu->banner_img = $baseUrl . '/' . $childMenu->banner_img;
+    //                     }
+    //                 });
+    //             });
+    
+    //             return $this->sendResponse($menus, 'Menu List For Instructor Retrieved Successfully.');
+    //         } else {
+    //             return $this->sendError('No menus found for the given language code.', 404);
+    //         }
+    // }
+    public function lang_slugs_wise(Request $request): JsonResponse{
         $data = $request->all();
         $menu_url = $data['menu_url'] ?? null;
        
@@ -421,7 +484,11 @@ class MenuController extends BaseController
                       ->orderBy('page_order', 'ASC');
             }])
             ->select(
+                'id',
+                'menu_type',
                 'menu_child_id',
+                'menu_position',
+                'language_id',
                 'menu_name',
                 'menu_url',
                 'menu_title',
@@ -430,6 +497,8 @@ class MenuController extends BaseController
                 'content',
                 'doc_upload',
                 'menu_links',
+                'page_order',
+                'current_version',
                 'welcomedescription',
                 'banner_img',
                 'img_upload'
