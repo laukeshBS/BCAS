ff1b9edf9332bc7fbe29a38b2ad6438fb1069a87
laukeshBS <laukesh.kumar@bharuwasolutions.com>
2024-11-12 15:54:54 +0530

    ajit code merges re d

--- admin/src/app/environments/environment.ts
+++ admin/src/app/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
 
     // apiBaseUrl: 'http://206.1.13.193:6002/bcas/api/',
-    apiBaseUrl: 'http://127.0.0.1:8000/api/',
+    apiBaseUrl: 'http://localhost/bcas/api/',
      // apiDocBaseUrl: 'http://206.1.13.193:6002/bcas/',
-    apiDocBaseUrl: 'http://127.0.0.1:8000/',
+    apiDocBaseUrl: 'http://localhost/bcas/',
   };
